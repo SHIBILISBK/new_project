e293777f6520f9260e1d1c48d030979a4d76632e
SHIBILISBK <shibilimohammed006@gmail.com>
2023-03-17 15:43:50 +0530

    commit

---
+++ lib/ui/TOURISM APP UI/tourist app ui 1/Tourpack.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+List<Map<String,dynamic>> tourpack=[{
+  'ID': 1,
+  'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0O3RnOZM9WfYpNLMApeUdLSEX8nMTmiTqAg&usqp=CAU',
+  'name': 'Munnar',
+  'description': 'Munnar is a town in the Western Ghats mountain range in India’s Kerala state.'
+      ' A hill station and former resort for the British Raj elite, it is surrounded by rolling hills dotted with'
+      ' tea plantations established in the late 19th century. Eravikulam National Park, a habitat for the endangered'
+      ' mountain goat Nilgiri tahr, is home to the Lakkam Waterfalls, hiking trails and 2,695m-tall Anamudi Peak.',
+},
+  {
+    'ID': 2,
+    'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTULJCKsoZSPLTxF1CgoqaUKUsMpU881eICFw&usqp=CAU',
+    'name': 'Wagamon',
+    'description': ''},
+  {
+    'ID': 3,
+    'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaSccRm11FCUb0QOBWhfO66eD7AHFhbPwgJw&usqp=CAU',
+    'name': 'Wayanad',
+    'description': ''},
+  {
+    'ID': 4,
+    'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJyWSY6xwOkODZDZx8FmLzQp7wpYrBwAd9MQ&usqp=CAU',
+    'name': 'Otty',
+    'description': ''},
+  {
+    'ID': 5,
+    'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUPJUN9FqZjZnsTiiT1I1jbkd4Fi7xru5uBQ&usqp=CAU',
+    'name': 'Kodaikanal',
+    'description': '' },
+  {
+    'ID': 6,
+    'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5CGDA9SJyr-cJZPxG-t8WGCeh2CP8z1nd9w&usqp=CAU',
+    'name': 'Idukki',
+    'description': ''},
+  {
+    'ID': 7 ,
+    'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9GOCuhXDwJiTn3TpATTAgtGOuv9P6qW0c8Q&usqp=CAU',
+    'name': 'Mysore',
+    'description': '' },
+];
