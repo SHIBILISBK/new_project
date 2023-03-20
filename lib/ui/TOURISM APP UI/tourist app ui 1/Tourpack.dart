11c2b876e02d48646182b44353cb1e997c775c1d
SHIBILISBK <shibilimohammed006@gmail.com>
2023-03-20 13:15:56 +0530

    commit

--- lib/ui/TOURISM APP UI/tourist app ui 1/Tourpack.dart
+++ lib/ui/TOURISM APP UI/tourist app ui 1/Tourpack.dart
@@ -13,30 +13,32 @@ List<Map<String,dynamic>> tourpack=[{
     'ID': 2,
     'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTULJCKsoZSPLTxF1CgoqaUKUsMpU881eICFw&usqp=CAU',
     'name': 'Wagamon',
-    'description': ''},
+    'description': 'Vagamon is an Indian hill station and a revenue village primarily located in Peerumedu taluk of Idukki district, and also Meenachil taluk and Kanjirappally taluk of Kottayam district in the state of Kerala, India.'},
   {
     'ID': 3,
     'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaSccRm11FCUb0QOBWhfO66eD7AHFhbPwgJw&usqp=CAU',
     'name': 'Wayanad',
-    'description': ''},
+    'description': 'Wayanad is a rural district in Kerala state, southwest India. In the east, the Wayanad Wildlife Sanctuary is a lush, forested region with areas of high altitude, home to animals including Asiatic elephants, tigers, leopards and egrets. In the Ambukuthi Hills to the south, Edakkal Caves contain ancient petroglyphs, some dating back to the Neolithic age'},
   {
     'ID': 4,
     'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJyWSY6xwOkODZDZx8FmLzQp7wpYrBwAd9MQ&usqp=CAU',
-    'name': 'Otty',
-    'description': ''},
+    'name': 'Ooty',
+    'description': 'Ooty (short for Udhagamandalam) is a resort town in the Western Ghats mountains, in southern India'
+        's Tamil Nadu state. Founded as a British Raj summer resort, it retains a working steam railway line. Other reminders of its colonial past include Stone House, a 19th-century residence, and the circa-1829 St. Stephen’s Church. Its 55-acre Government Botanical Garden lies on the slopes of Doddabetta Peak.'},
   {
     'ID': 5,
     'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUPJUN9FqZjZnsTiiT1I1jbkd4Fi7xru5uBQ&usqp=CAU',
     'name': 'Kodaikanal',
-    'description': '' },
+    'description': 'Kodaikanal is a hill town in the southern Indian state of Tamil Nadu. It’s set in an area of granite cliffs, forested valleys, lakes, waterfalls and grassy hills. At 2,000 meters above sea level, the town centers around man-made, star-shaped Kodaikanal Lake, bordered by evergreen forest. Rowing boats can be hired, and hikers and cyclists follow the 5k Lake Road path around the shore. ' },
   {
     'ID': 6,
     'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5CGDA9SJyr-cJZPxG-t8WGCeh2CP8z1nd9w&usqp=CAU',
     'name': 'Idukki',
-    'description': ''},
+    'description': 'Idukki district is a densely forested, mountainous region in the south Indian state of Kerala. In the north, Anamudi mountain towers over Eravikulam National Park, where the rare, blue Neelakurinji flower blooms every 12 years. Nearby, Munnar is a hill station known for its sprawling tea plantations and Tea Museum. Farther south is the vast, curved Idukki Dam and Periyar National Park, a tiger and elephant reserve. '},
   {
     'ID': 7 ,
     'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9GOCuhXDwJiTn3TpATTAgtGOuv9P6qW0c8Q&usqp=CAU',
     'name': 'Mysore',
-    'description': '' },
+    'description': 'Mysore (or Mysuru), a city in India'
+'s southwestern Karnataka state, was the capital of the Kingdom of Mysore from 1399 to 1947. In its center is opulent Mysore Palace, seat of the former ruling Wodeyar dynasty. The palace blends Hindu, Islamic, Gothic and Rajput styles. Mysore is also home to the centuries-old Devaraja Market, filled with spices, silk and sandalwood.' },
 ];
